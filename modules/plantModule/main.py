7467a4d073480ff9e1a196bace08c198c7797943
gc-zoetis <gaurav.choubey@zoetis.com>
2023-04-10 10:26:03 +0530

    resolved the delay in plantmodule from adc

--- modules/plantModule/main.py
+++ modules/plantModule/main.py
@@ -10,26 +10,35 @@ from azure.iot.device.aio import IoTHubModuleClient
 import json
 import random
 
+
 # Pi Dependencies
 from time import time, sleep, strftime
 import board
 import busio
 import adafruit_dht
-import adafruit_ads1x15.ads1115 as ADS
-from adafruit_ads1x15.analog_in import AnalogIn
+# import adafruit_ads1x15.ads1115 as ADS
+import Adafruit_ADS1x15
+
+# from adafruit_ads1x15.analog_in import AnalogIn
 
 dhtDevice = adafruit_dht.DHT22(board.D4)
-i2c = busio.I2C(board.SCL, board.SDA)
-ads = ADS.ADS1115(i2c)
+# i2c = busio.I2C(board.SCL, board.SDA)
+# ads = ADS.ADS1115(i2c)
+ads=Adafruit_ADS1x15.ADS1115()
 
-plant = AnalogIn(ads, ADS.P1)
-ldr = AnalogIn(ads, ADS.P3)
+# plant = AnalogIn(ads, ADS.P1)
+# ldr = AnalogIn(ads, ADS.P3)
 
 # Event indicating client stop
 stop_event = threading.Event()
 
+#variable
 plantMessage={}
 plantMessageJson=None
+
+#
+MAX_COUNT=20
+GAIN = 1
 
 def getPlantData():
     # capture the all data from sensor 
@@ -38,15 +47,33 @@ def getPlantData():
     try:
         plant_val = 0
         ldr_val = 0
+
+        #temperature and humidity
         temperature = dhtDevice.temperature
         humidity = dhtDevice.humidity
-        for i in range (0, 50):
-            plant_val = plant.value + plant_val
-            ldr_val = ldr.value + ldr_val 
+
+        #plant value capturing
+        beforeTime=time()*1000
+
+        for i in range (0, MAX_COUNT):
+            plant_val += ads.read_adc(1, gain=GAIN)
+
+        plant_val = plant_val/MAX_COUNT
+
+        afterTime=time()*1000
+        print("Plant Value :{} in time :{} ms of avg of {}".format(plant_val,afterTime-beforeTime,MAX_COUNT))
+        sleep(0.1)
+
+        #ldr value capture
+        beforeTime=time()*1000
+        for i in range (0, MAX_COUNT):
+            ldr_val += ads.read_adc(3, gain=GAIN)
+
+        ldr_val = ldr_val/MAX_COUNT
+        afterTime=time()*1000
+        print("Plant Value :{} in time :{} ms of avg of {}".format(ldr_val,afterTime-beforeTime,MAX_COUNT))
         
-        plant_val = plant_val/50
-        ldr_val = ldr_val/50
-        
+        #form the json payload
         #{"plantSignal": 1129,"temperature": 20.0,"humidity": 60,"luminosity": 778,"plantMood": Normal}
         plantMessage["plantSignal"] = plant_val
         plantMessage["temperature"] = temperature
@@ -92,7 +119,8 @@ async def run_sample(client):
         if plantMessageJson is not None:
             print("data in the message created on input1: ",plantMessageJson)
             await client.send_message_to_output(plantMessageJson, "output1")
-        await asyncio.sleep(5)
+
+        await asyncio.sleep(0.01)
 
 def main():
     if not sys.version >= "3.5.3":
